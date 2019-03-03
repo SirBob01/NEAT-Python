95978e8ae02ad1681e852a2843e78a83475db89b
Keith Leonardo <kleonardo52@gmail.com>
2019-03-03 20:01:41 +1100

    Update neat.py

--- neat/neat.py
+++ neat/neat.py
@@ -271,7 +271,7 @@ class Brain(object):
 	"""Base class for a 'brain' that learns through the evolution
 	of a population of genomes.
 	"""
-	def __init__(self, inputs, outputs,	population=100,  max_fitness=-1, max_generations=-1, delta_threshold=3, cull_percent=0.75):
+	def __init__(self, inputs, outputs, population=100, max_fitness=-1, max_generations=-1, delta_threshold=3, cull_percent=0.75):
 		self._inputs = inputs
 		self._outputs = outputs
 		self._edges = [] # Edge database (INPUT, OUTPUT)
