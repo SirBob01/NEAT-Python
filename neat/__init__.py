fa386b033f5857d7800bd3842bede8c9e73cfd2c
Keith Leonardo <kleonardo52@gmail.com>
2019-03-03 16:05:53 +1100

    Update __init__.py

--- neat/__init__.py
+++ neat/__init__.py
@@ -1,12 +1,15 @@
-"""NEAT (NeuroEvolution of Augmenting Topologies) is a genetic algorithm 
-developed by Ken Stanley that applies genetic algorithms to machine learning.
-This algorithm gradually breeds and mutates the best genomes (neural networks)
-with the highest fitness or performance scores over the course of generations.
+"""NEAT (NeuroEvolution of Augmenting Topologies) is a 
+genetic algorithm developed by Ken Stanley that applies 
+genetic algorithms to machine learning. This algorithm 
+gradually breeds and mutates the best genomes (neural 
+networks) with the highest fitness or performance scores 
+over the course of generations.
 
 	http://nn.cs.utexas.edu/downloads/papers/stanley.ec02.pdf
 
-This is a Python implementation of the algorithm for use in simulations, 
-games, or general machine learning problems.
+This is a Python implementation of the algorithm for 
+use in simulations, games, or general machine learning 
+problems.
 """
 __all__ = ['neat']
 
