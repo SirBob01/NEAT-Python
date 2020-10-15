f783a7548bf8284bf301215a6c86a193b2ddde76
Keith Leonardo <kleonardo52@gmail.com>
2020-10-16 03:10:47 +1100

    Update XOR problem demo

--- demo/xor.py
+++ demo/xor.py
@@ -10,42 +10,44 @@ def fitness(expected, output):
 
 
 def main():
-    if os.path.isfile('xor.neat'):
-        brain = neat.Brain.load('xor')
-    else:
-        brain = neat.Brain(2, 1, population=100, max_generations=1000)
-        brain.generate()
+    brain = neat.Brain(2, 1, population=150, max_generations=300)
+    brain.generate()
     
-    if brain.should_evolve():
-        print("Training...")
-    else:
-        print("Final model...")
-
+    print("Training...")
+    last_gen = None
     while brain.should_evolve():
         genome = brain.get_current()
+
         f1 = genome.forward([0.0, 0.0])[0]
         f2 = genome.forward([1.0, 0.0])[0]
         f3 = genome.forward([0.0, 1.0])[0]
         f4 = genome.forward([1.0, 1.0])[0]
-        fit = fitness([0.0, 1.0, 1.0, 0.0], [f1, f2, f3, f4])
+        genome.set_fitness(fitness([0.0, 1.0, 1.0, 0.0], [f1, f2, f3, f4]))
 
-        genome.set_fitness(fit)
+        # Print training progress
+        current_gen = brain.get_generation()
+        if last_gen != current_gen:
+            current_best = brain.get_fittest()
+            print("Current Accuracy: {:.2f}% | Generation {}/{}".format(
+                current_best.get_fitness() * 100, 
+                current_gen + 1, 
+                brain._max_generations
+            ))
+            last_gen = current_gen
         brain.next_iteration()
 
-        if fit > brain.get_fittest().get_fitness():
-            # Values should progressively approach [0, 1, 1, 0]
-            print("[{:.3f} {:.3f} {:.3f} {:.3f}] | Acc.: {:.2f}% | Gen.: {:d}".format(
-                f1, f2, f3, f4, (fit * 100.0), brain.get_generation())
-            )
-
-        brain.save('xor')
-
     best = brain.get_fittest()
-    print("Accuracy: {:.2f}%".format(best.get_fitness() * 100))
-    print("0 ^ 0 = {:.3f}".format(best.forward([0.0, 0.0])[0]))
-    print("1 ^ 0 = {:.3f}".format(best.forward([1.0, 0.0])[0]))
-    print("0 ^ 1 = {:.3f}".format(best.forward([0.0, 1.0])[0]))
-    print("1 ^ 1 = {:.3f}".format(best.forward([1.0, 1.0])[0]))
+    f1 = best.forward([0.0, 0.0])[0]
+    f2 = best.forward([1.0, 0.0])[0]
+    f3 = best.forward([0.0, 1.0])[0]
+    f4 = best.forward([1.0, 1.0])[0]
+    fit = fitness([0.0, 1.0, 1.0, 0.0], [f1, f2, f3, f4])
+
+    print("Accuracy: {:.2f}%".format(fit * 100))
+    print("0 ^ 0 = {:.3f}".format(f1))
+    print("1 ^ 0 = {:.3f}".format(f2))
+    print("0 ^ 1 = {:.3f}".format(f3))
+    print("1 ^ 1 = {:.3f}".format(f4))
 
 if __name__ == "__main__":
     main()
